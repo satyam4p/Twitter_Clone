9784b2b65892b34d67ffb7b3def1a681bfb724a8
Satyam Kumar <satyamkumar343@gmail.com>
2020-12-13 18:25:29 +0530

    layout-changes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("he;llo world!");
+const twitterIcon = document.querySelector('.twiiter_icon');
+const drawer = document.querySelector('.drawer');
+
+twitterIcon.addEventListener("click",()=>{
+    console.log("hello world!!");
+    drawer.classList.toggle("animateClass");
+    twitterIcon.classList.toggle("fade");
+    
+})
